0f47545c4fe7f0914f4ab79f44e313d508c7a75b
EngenMe <mohamed_farouk.hasnaoui@g.enp.edu.dz>
2024-09-21 15:13:05 +0100

    Create reverse string solution - week1-pb2.

--- Level-1-Foundations-Easy/Week-1-Arrays-and-Strings/twoSum.js
+++ Level-1-Foundations-Easy/Week-1-Arrays-and-Strings/twoSum.js
@@ -1,4 +1,4 @@
-function findIndices(nums, target) {
+function twoSum(nums, target) {
   const numMap = {};
   for (let i = 0; i < nums.length; i++) {
     const complement = target - nums[i];
@@ -8,6 +8,6 @@ function findIndices(nums, target) {
   }
 }
 
-console.log(findIndices([2, 7, 11, 15], 9));
-console.log(findIndices([3, 2, 4], 6));
-console.log(findIndices([3, 3], 6));
+console.log(twoSum([2, 7, 11, 15], 9));
+console.log(twoSum([3, 2, 4], 6));
+console.log(twoSum([3, 3], 6));
